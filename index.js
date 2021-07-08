207d2512ee6b054c53d20f62a7bd10ef0d8a5e0a
Lauren Hess <laurenbethhess@gmail.com>
2021-07-08 08:08:23 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Lauren Hess",
+    streetAddress: "5 Busdick Drive"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const anotherNewEmployee = Object.assign({}, employee)
+    delete anotherNewEmployee[key];
+    return anotherNewEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const anotherNewEmployee = Object.assign(employee)
+    delete anotherNewEmployee[key];
+    return anotherNewEmployee;
+}
